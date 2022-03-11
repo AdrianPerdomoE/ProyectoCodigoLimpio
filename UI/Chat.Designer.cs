ce30713681d24b2285bad46404cffc689be0b3c4
Fasbox <fabianchavarria03@gmail.com>
2022-03-11 16:07:40 -0500

    Sincro 2

---
+++ UI/Chat.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TrackerUI
+{
+    partial class Chat
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.bottomChatPanel = new System.Windows.Forms.Panel();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.messagesSentListView = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.headerPanel.SuspendLayout();
+            this.bottomChatPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(57)))), ((int)(((byte)(110)))));
+            this.headerPanel.Controls.Add(this.exitButton);
+            this.headerPanel.Controls.Add(this.headerLabel);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(957, 32);
+            this.headerPanel.TabIndex = 0;
+            this.headerPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.headerPanel_MouseDown);
+            this.headerPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.headerPanel_MouseMove);
+            this.headerPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.headerPanel_MouseUp);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(57)))), ((int)(((byte)(110)))));
+            this.exitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.exitButton.ForeColor = System.Drawing.Color.White;
+            this.exitButton.Location = new System.Drawing.Point(898, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(59, 32);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "X";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.headerLabel.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.headerLabel.ForeColor = System.Drawing.Color.White;
+            this.headerLabel.Location = new System.Drawing.Point(12, 2);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(66, 28);
+            this.headerLabel.TabIndex = 1;
+            this.headerLabel.Text = "Chat";
+            this.headerLabel.Click += new System.EventHandler(this.headerLabel_Click);
+            // 
+            // bottomChatPanel
+            // 
+            this.bottomChatPanel.Controls.Add(this.sendButton);
+            this.bottomChatPanel.Controls.Add(this.textBox);
+            this.bottomChatPanel.Location = new System.Drawing.Point(57, 507);
+            this.bottomChatPanel.Name = "bottomChatPanel";
+            this.bottomChatPanel.Size = new System.Drawing.Size(843, 47);
+            this.bottomChatPanel.TabIndex = 1;
+            // 
+            // sendButton
+            // 
+            this.sendButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(57)))), ((int)(((byte)(110)))));
+            this.sendButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.sendButton.FlatAppearance.BorderSize = 0;
+            this.sendButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sendButton.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.sendButton.ForeColor = System.Drawing.Color.White;
+            this.sendButton.Location = new System.Drawing.Point(780, 0);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(63, 45);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = ">";
+            this.sendButton.UseVisualStyleBackColor = false;
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(34)))), ((int)(((byte)(58)))));
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Font = new System.Drawing.Font("Segoe UI", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox.ForeColor = System.Drawing.Color.White;
+            this.textBox.Location = new System.Drawing.Point(0, 1);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(763, 44);
+            this.textBox.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.messagesSentListView);
+            this.panel2.Location = new System.Drawing.Point(57, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(843, 428);
+            this.panel2.TabIndex = 2;
+            // 
+            // messagesSentListView
+            // 
+            this.messagesSentListView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(34)))), ((int)(((byte)(58)))));
+            this.messagesSentListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.messagesSentListView.ForeColor = System.Drawing.Color.White;
+            this.messagesSentListView.Location = new System.Drawing.Point(0, 0);
+            this.messagesSentListView.Name = "messagesSentListView";
+            this.messagesSentListView.Size = new System.Drawing.Size(843, 428);
+            this.messagesSentListView.TabIndex = 0;
+            this.messagesSentListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Chat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(18)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(957, 577);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.bottomChatPanel);
+            this.Controls.Add(this.headerPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Chat";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chat";
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            this.bottomChatPanel.ResumeLayout(false);
+            this.bottomChatPanel.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel headerPanel;
+        private Label headerLabel;
+        private Button exitButton;
+        private Panel bottomChatPanel;
+        private TextBox textBox;
+        private Panel panel2;
+        private ListView messagesSentListView;
+        private Button sendButton;
+        private Button button1;
+    }
+}
---
+++ UI/Chat.cs
@@ -0,0 +1,54 @@
+namespace TrackerUI
+{
+    public partial class Chat : Form
+    {
+        public Chat()
+        {
+            InitializeComponent();
+        }
+
+        private void headerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        int mouseX = 0;
+        int mouseY = 0;
+        int mouseinX = 0;
+        int mouseinY = 0;
+        bool mouseDown;
+        private void headerPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+
+            mouseinX = MousePosition.X - Bounds.X;
+            mouseinY = MousePosition.Y - Bounds.Y;
+        }
+
+        private void headerPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mouseDown)
+            {
+                mouseX = MousePosition.X - mouseinX;
+                mouseY = MousePosition.Y - mouseinY;
+
+                this.SetDesktopLocation(mouseX, mouseY);
+            }
+        }
+
+        private void headerPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
