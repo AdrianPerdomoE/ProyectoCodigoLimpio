07467c971c71dbb0cb877a99db79d8b7aee49227
Fasbox <fabianchavarria03@gmail.com>
2022-03-11 16:19:57 -0500

    Commit 5

--- UI/Chat.Designer.cs
+++ UI/Chat.Designer.cs
@@ -36,7 +36,6 @@
             this.textBox = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.messagesSentListView = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
             this.headerPanel.SuspendLayout();
             this.bottomChatPanel.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -139,23 +138,12 @@
             this.messagesSentListView.TabIndex = 0;
             this.messagesSentListView.UseCompatibleStateImageBehavior = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 53);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Chat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(18)))), ((int)(((byte)(31)))));
             this.ClientSize = new System.Drawing.Size(957, 577);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.bottomChatPanel);
             this.Controls.Add(this.headerPanel);
@@ -182,6 +170,5 @@
         private Panel panel2;
         private ListView messagesSentListView;
         private Button sendButton;
-        private Button button1;
     }
 }
